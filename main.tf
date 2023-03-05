fdf303c70327b78dc55b6d43505f5a5f7554d52e
Vivek0218 <105193786+Vivek0218@users.noreply.github.com>
2023-03-05 19:12:25 +0530

    Commit 1

--- main.tf
+++ main.tf
@@ -11,6 +11,9 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
+  use_msi = true
+  subscription_id = "a22c09bc-5167-4466-b7ef-d15f092a8b94"
+  tenant_id = "4d998499-a954-43fe-9f1e-8c593eea1fe1"
 }
 
 # Create a resource group
@@ -21,8 +24,13 @@ resource "azurerm_resource_group" "demo01" {
 
 # Create a Virtual Network
 resource "azurerm_virtual_network" "vnet01" {
-  name = ""
+  name = "test_vnet01"
   resource_group_name = azurerm_resource_group.demo01.name
   location = azurerm_resource_group.demo01.location
-  address_space = "10.10.0.0/16"
+  address_space = ["10.10.0.0/16"]
+
+  subnet {
+    name = "sub01"
+    address_prefix = "10.10.1.0/24"
+  }
 }
